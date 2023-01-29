c0e549db50379c91ab5948c1a3ff635fcd4569ea
odow <o.dowson@gmail.com>
2023-01-30 12:04:46 +1300

    Add more support for logical operators

--- test/runtests.jl
+++ test/runtests.jl
@@ -1134,6 +1134,33 @@ function test_model_nlp_boolean_jump()
     return
 end
 
+function test_model_nlp_boolean_registered()
+    model = MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Int}())
+    x = MOI.add_variables(model, 3)
+    MOI.add_constraint.(model, x, MOI.ZeroOne())
+    nlp = MOI.Nonlinear.Model()
+    f(args...) = error("evaluation not supported")
+    MOI.Nonlinear.register_operator(nlp, :!, 1, f, f, f)
+    MOI.Nonlinear.register_operator(nlp, :(=>), 2, f, f, f)
+    a, b, c = x
+    MOI.Nonlinear.add_constraint(nlp, :(($a => !($b || $c))), MOI.EqualTo(1.0))
+    backend = MOI.Nonlinear.ExprGraphOnly()
+    evaluator = MOI.Nonlinear.Evaluator(nlp, backend, x)
+    MOI.set(model, MOI.NLPBlock(), MOI.NLPBlockData(evaluator))
+    solver = MiniZinc.Optimizer{Int}(MiniZinc.Chuffed())
+    MOI.set(solver, MOI.RawOptimizerAttribute("model_filename"), "test.mzn")
+    index_map, _ = MOI.optimize!(solver, model)
+    @test MOI.get(solver, MOI.TerminationStatus()) === MOI.OPTIMAL
+    @test MOI.get(solver, MOI.ResultCount()) >= 1
+    y = [index_map[v] for v in x]
+    sol = round.(Bool, MOI.get(solver, MOI.VariablePrimal(), y))
+    @test ifelse(sol[1], !(sol[2] || sol[3]), true)
+    @test read("test.mzn", String) ==
+          "var bool: x1;\nvar bool: x2;\nvar bool: x3;\nconstraint (x1 -> not((x2 \\/ x3))) == true;\nsolve satisfy;\n"
+    rm("test.mzn")
+    return
+end
+
 function test_model_solver_name()
     solver = MiniZinc.Optimizer{Int}(MiniZinc.Chuffed())
     @test MOI.get(solver, MOI.SolverName()) == "MiniZinc"
