55be81170fdd3edc81cb568d028300034840d154
odow <o.dowson@gmail.com>
2023-11-17 15:33:27 +1300

    Trap non-zero process return values in _run_minizinc

--- src/optimize.jl
+++ src/optimize.jl
@@ -77,16 +77,25 @@ function _run_minizinc(dest::Optimizer)
     end
     output = joinpath(dir, "model.ozn")
     _stdout = joinpath(dir, "_stdout.txt")
-    _minizinc_exe() do exe
-        cmd = `$(exe) --solver $(dest.solver) --output-objective -o $(output) $(filename)`
-        if dest.time_limit_sec !== nothing
-            limit = round(Int, 1_000 * dest.time_limit_sec::Float64)
-            cmd = `$cmd --time-limit $limit`
+    _stderr = joinpath(dir, "_stderr.txt")
+    try
+        _minizinc_exe() do exe
+            cmd = `$(exe) --solver $(dest.solver) --output-objective -o $(output) $(filename)`
+            if dest.time_limit_sec !== nothing
+                limit = round(Int, 1_000 * dest.time_limit_sec::Float64)
+                cmd = `$cmd --time-limit $limit`
+            end
+            if dest.options["num_solutions"] > 1
+                cmd = `$cmd --num-solutions $(dest.options["num_solutions"])`
+            end
+            return run(pipeline(cmd, stdout = _stdout, stderr = _stderr))
         end
-        if dest.options["num_solutions"] > 1
-            cmd = `$cmd --num-solutions $(dest.options["num_solutions"])`
+    catch
+        status = "=====ERROR=====\n"
+        if isfile(_stderr)
+            status *= read(_stderr, String)
         end
-        return run(pipeline(cmd, stdout = _stdout))
+        return status
     end
     if isfile(output)
         return read(output, String)
@@ -196,9 +205,9 @@ function MOI.optimize!(dest::Optimizer{T}, src::MOI.ModelLike) where {T}
         m_stat = match(r"=====(.+)=====", ret)
         if m_stat !== nothing
             @assert length(m_stat.captures) == 1
-            dest.solver_status = m_stat[1]
+            dest.solver_status = ret
         else
-            dest.solver_status = "SATISFIABLE"
+            dest.solver_status = "=====SATISFIABLE====="
             variable_map = Dict(
                 MOI.get(dest.inner, MOI.VariableName(), x) => x for
                 x in MOI.get(src, MOI.ListOfVariableIndices())
@@ -232,14 +241,14 @@ function MOI.is_valid(model::Optimizer, x::MOI.VariableIndex)
 end
 
 function _has_solution(model::Optimizer)
-    return model.solver_status == "SATISFIABLE" &&
+    return model.solver_status == "=====SATISFIABLE=====" &&
            !isempty(model.primal_solutions)
 end
 
 MOI.get(model::Optimizer, ::MOI.RawStatusString) = model.solver_status
 
 function MOI.get(model::Optimizer, ::MOI.TerminationStatus)
-    if model.solver_status == "UNSATISFIABLE"
+    if occursin("=UNSATISFIABLE=", model.solver_status)
         return MOI.INFEASIBLE
     elseif _has_solution(model)
         if 1 < model.options["num_solutions"] <= length(model.primal_solutions)
@@ -247,7 +256,7 @@ function MOI.get(model::Optimizer, ::MOI.TerminationStatus)
         else
             return MOI.OPTIMAL
         end
-    elseif model.solver_status == "UNKNOWN" &&
+    elseif occursin("UNKNONWN", model.solver_status) &&
            model.time_limit_sec !== nothing &&
            model.solve_time_sec >= model.time_limit_sec
         return MOI.TIME_LIMIT   # The solver timed out
