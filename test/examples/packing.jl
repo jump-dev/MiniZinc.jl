d571efa826c41b8932b031baf11ea8efe94ac35b
Oscar Dowson <odow@users.noreply.github.com>
2024-09-03 09:03:41 +1200

    Update test/examples/packing.jl

--- test/examples/packing.jl
+++ test/examples/packing.jl
@@ -1,3 +1,8 @@
+# Copyright (c) 2022 MiniZinc.jl contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 # Inspired from the square packing tutorial in https://www.minizinc.org/
 function test_packing()
     # Number of squares
